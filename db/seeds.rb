90a84fb9fad1b63617b8b74d65b02e570e60a903
Daniel Merys <dkmerys@gmail.com>
2020-06-12 08:53:00 -0700

    seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,23 +8,25 @@ class Seed
   def generate_animals
     25.times do |i|
       animal = Animal.create!(
-        name: Faker::Creature::Dog.name
-        breed: Faker::Creature::Dog.breed
-        age: Faker::Number.within(range: 1..10)
-        gender: Faker::Gender.binary
+        name: Faker::Creature::Dog.name,
+        breed: Faker::Creature::Dog.breed,
+        age: Faker::Number.within(range: 1..10),
+        gender: Faker::Gender.binary_type,
         quote: Faker::Quote::most_interesting_man_in_the_world
       )
       puts "Animal #{i}: Name is #{animal.name}, breed is #{animal.breed}, age is #{animal.age}, gender is #{animal.gender}, and quote is '#{animal.quote}'."
     end
     25.times do |i|
       animal = Animal.create!(
-        name: Faker::Creature::Cat.name
-        breed: Faker::Creature::Cat.breed
-        age: Faker::Number.within(range: 1..14)
-        gender: Faker::Gender.binary
+        name: Faker::Creature::Cat.name,
+        breed: Faker::Creature::Cat.breed,
+        age: Faker::Number.within(range: 1..14),
+        gender: Faker::Gender.binary_type,
         quote: Faker::Quote::most_interesting_man_in_the_world
       )
       puts "Animal #{i}: Name is #{animal.name}, breed is #{animal.breed}, age is #{animal.age}, gender is #{animal.gender}, and quote is '#{animal.quote}'."
     end
   end
 end
+
+Seed.begin
